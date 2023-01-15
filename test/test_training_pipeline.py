6f36a40bb0881229c8dd401b1c6aaa5a8d80e5e1
Marcel <marcel-ju@web.de>
2023-01-15 10:29:01 +0100

    Fix test

--- test/test_training_pipeline.py
+++ test/test_training_pipeline.py
@@ -10,17 +10,17 @@ if local:
 def test_full_training(use_bayesian_search, bayesian_n_iterations):
     model, X_train, X_test, y_train, y_test = train_model(use_bayesian_search, bayesian_n_iterations, use_gpu=False)
 
-    assert model is not None
-    assert X_train is not None
-    assert X_test is not None
-    assert y_train is not None
-    assert y_test is not None
+    assert model is not None, "model must not be None"
+    assert X_train is not None, "X_train must not be None"
+    assert X_test is not None, "X_test must not be None"
+    assert y_train is not None, "y_train must not be None"
+    assert y_test is not None, "y_test must not be None"
 
     y_pred = model.predict(X_test)
     y_pred = post_process_predictions(y_pred)
-    assert y_pred is not None
+    assert y_pred is not None, "y_pred must not be None"
 
     # Upvote ratio can only be in interval [0,1]
-    upvote_ratio_idx = y_test.columns.get_loc("upvote_ratio")
-    assert y_pred[upvote_ratio_idx].min() >= 0
-    assert y_pred[upvote_ratio_idx].max() <= 1
+    upvote_ratio_idx = 1
+    assert y_pred[upvote_ratio_idx, :].min() >= 0, "upvote ratio must be >= 0"
+    assert y_pred[upvote_ratio_idx, :].max() <= 1, "upvote ratio must be <= 1"
